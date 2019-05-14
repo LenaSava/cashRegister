2a9beea8c77dfd9f1a05db5512f9eb9c42a14ca4
Olena Savinkova <owlindacity@gmail.com>
2019-05-14 21:21:06 +0200

    add invoice

--- src/main/java/model/entity/Invoice.java
+++ src/main/java/model/entity/Invoice.java
@@ -1,4 +1,63 @@
 package model.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Invoice {
+    private Integer id;
+    private Double cost;
+    private Double quantity;
+    private Integer userId;
+    private User user;
+
+    private List<Product> products = new ArrayList<>();
+
+    private Invoice(Integer id, Double cost, Double quantity,Integer userId, User user) {
+        this.id = id;
+        this.cost = cost;
+        this.quantity = quantity;
+        this.userId = userId;
+        this.user = user;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public Double getCost() {
+        return cost;
+    }
+    public void setCost(Double cost) {
+        this.cost = cost;
+    }
+    public Double getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(Double quantity) {
+        this.quantity = quantity;
+    }
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+    public void settProducts(List<Product> products) {
+        this.products = products;
+    }
 }
