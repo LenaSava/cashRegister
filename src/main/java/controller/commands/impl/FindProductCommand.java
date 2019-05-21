57f42138b95c4409ab9aa2b368dc1c529d336782
Olena Savinkova <owlindacity@gmail.com>
2019-05-21 13:27:37 +0300

    update find product command

--- src/main/java/controller/commands/impl/FindProductCommand.java
+++ src/main/java/controller/commands/impl/FindProductCommand.java
@@ -10,16 +10,21 @@ import java.util.Optional;
 
 public class FindProductCommand implements Command {
     @Override
-    public String execute(HttpServletRequest request, HttpServletResponse response) {
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws NumberFormatException {
         String codeFromRequest = request.getParameter("search");
+
+        try {
+
         Optional<Product> findCode = productService.findByCode(Integer.parseInt(codeFromRequest));
 
-    if(findCode.isPresent()) {
-        request.setAttribute("products", Arrays.asList(findCode.get()));
-    } else {
-        request.setAttribute("errorMessage", "product with such code not found!");
-    }
-
+            if (findCode.isPresent()) {
+                request.setAttribute("products", Arrays.asList(findCode.get()));
+            } else {
+                request.setAttribute("errorMessage", "product with such code not found!");
+            }
+        } catch (NumberFormatException e) {
+            request.setAttribute("products", productService.getAllProducts());
+        }
         return CAHIER_PAGE_JSP;
     }
 }
