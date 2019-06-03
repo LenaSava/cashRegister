cde8e5a84a6c6b92d710696cda9f9769cd6112ab
Olena Savinkova <owlindacity@gmail.com>
2019-06-03 13:10:42 +0300

    refactoring

--- src/main/java/model/service/GerericService.java
+++ src/main/java/model/service/GerericService.java
@@ -1,4 +1,11 @@
 package model.service;
 
-public class GerericService {
+import java.util.List;
+
+public interface GerericService<T> {
+    boolean create (T entity);
+    T findById(int id);
+    List<T> findAll();
+    void update(T entity);
+    boolean delete(int id);
 }
